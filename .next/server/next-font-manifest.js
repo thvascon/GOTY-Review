5b2733aa9f007277e0303a1ab5c565203e2c48f8
root <thiago.moreira@ligo.cloud>
2025-10-06 15:20:56 -0300

    removing next.js files

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/Estudo/Site Games Score/game-review-squad-main/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/eafabf029ad39a43-s.p.woff2\",\"static/media/0484562807a97172-s.p.woff2\",\"static/media/b957ea75a84b6ea7-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
